36a7b84721f71723ed742932a46167b0bb264e16
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-04-10 10:54:45 +0200

    Mod: Reworked Request

--- src/api/helpers/Request.ts
+++ src/api/helpers/Request.ts
@@ -1,22 +1,69 @@
 type ApiResponse = {
+    ok: boolean,
+    status: number,
+    statusText: string,
     data: any,
     meta: Object
 }
 
-// Typical Request:
-// data: Object | Array
-// meta: Object
-// error: message | null ?
+type ApiResponseBody = {
+    data: any,
+    meta: Object
+}
 
 export default class Request {
     static async get(url: URL | RequestInfo, options: RequestInit = defaultOptions): Promise<ApiResponse> {
+        const mergedOptions = {
+            ...defaultOptions,
+            ...options
+        }
+
         try {
-            const response = await fetch(API_URL + url, {
-                ...defaultOptions,
-                ...options
-            });
+            const response = await fetch(API_URL + url, mergedOptions);
+            const { ok, status, statusText } = response;
+
+            const responseInfos = { ok, status, statusText, data: null, meta: {} };
+
+            if (!ok || status === 204)
+                return responseInfos;
             
-            return await response.json();
+            const responseBody: ApiResponseBody = await response.json();
+
+            return {
+                ...responseInfos,
+                ...responseBody,
+            };
+        } catch (error) {
+            console.log(`\x1b[33mError while fetching with url : ${url}\x1b[37m`);
+            console.log(error);
+        }
+
+        return defaultResponse;
+    }
+
+    static async make(url: URL | RequestInfo, method: string, data?: Object, options: RequestInit = defaultOptions): Promise<ApiResponse> {
+        const mergedOptions: RequestInit = {
+            ...defaultOptions,
+            ...options,
+            method,
+            body: JSON.stringify(data)
+        };
+        
+        try {
+            const response = await fetch(API_URL + url, mergedOptions);
+            const { ok, status, statusText } = response;
+
+            const responseInfos = { ok, status, statusText, data: null, meta: {} };
+
+            if (!ok || status === 204)
+                return responseInfos;
+
+            const responseBody: ApiResponseBody = await response.json();
+
+            return {
+                ...responseInfos,
+                ...responseBody,
+            };
         } catch (error) {
             console.log(`\x1b[33mError while fetching with url : ${url}\x1b[37m`);
             console.log(error);
