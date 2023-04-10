d7d2c0eb445e9462165f693180fec9ef48fa963a
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-04-10 22:52:05 +0200

    Feat: Account menu

---
+++ src/components/Header/AccountMenu.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from '..';
+
+const AccountMenu = () => {
+    return (
+        <div className='account-menu'>
+            <ul>
+                <li>
+                    <Link href='/dashboard'>
+                        Mes cours
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/parametres'>
+                        Paramètres
+                    </Link>
+                </li>
+                <li>
+                    <button>
+                        Déconnexion
+                    </button>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default AccountMenu;
