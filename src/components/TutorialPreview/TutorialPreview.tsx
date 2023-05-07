f5e10001123316af5a84d9bff331b1aa4d5f77ad
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-05-07 11:43:57 +0200

    Feat: Tutorial Preview

---
+++ src/components/TutorialPreview/TutorialPreview.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { GrTechnology } from 'react-icons/gr';
+import { AiOutlineClockCircle } from 'react-icons/ai';
+
+interface TutorialPreviewProps {
+    slug: string
+    title: string
+    description: string
+    technology: string
+    hoursToLearn: number
+}
+
+const TutorialPreview = ({ slug, title, description, technology, hoursToLearn }: TutorialPreviewProps) => {
+    return (
+        <a href={"/tutoriels/" + slug} className="course">
+            <img src="" alt="" />
+            <div className="infos">
+                <h3>{title}</h3>
+                <div className="row">
+                    <GrTechnology />
+                    <p>{technology}</p>
+                    <AiOutlineClockCircle />
+                    <p>{hoursToLearn} Heures</p>
+                </div>
+                <p className="desc">{description}</p>
+            </div>
+        </a>
+    );
+};
+
+export default TutorialPreview;
---
+++ src/components/TutorialPreview/index.ts
@@ -0,0 +1,1 @@
+export { default as TutorialPreview } from './TutorialPreview';
