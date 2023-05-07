5cb1c1b4f03a6f640cb5a0734c8e2725ac3076a0
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-05-07 11:46:45 +0200

    Feat: Tutorial Creation Page

--- src/pages/admin/new-tutorial/index.tsx
+++ src/pages/admin/new-tutorial/index.tsx
@@ -2,18 +2,27 @@ import { Breadcrumb, PageLayout } from '@/components'
 import { SeoModel } from '@/api/models'
 import { GetServerSideProps } from 'next'
 import { Request } from '@/api'
-import { FormField } from '@/components/FormField'
+import { FormCheckbox, FormField, FormNumber, FormTextArea } from '@/components/FormField'
 import { useState } from 'react'
+import { FormSelect } from '@/components/FormSelect'
 
 const NewTutorial = () => {
 
     const [newTutorial, setNewTutorial] = useState(defaultNewTutorial);
 
-    const handleChangeTutorial = (name: string, value: string) => {
+    const handleChangeTutorial = (name: string, value: string | boolean | number) => {
         setNewTutorial({
             ...newTutorial,
             [name]: value
         });
+    }
+
+    const handleSubmitTutorial = async () => {
+        const response = await Request.make('/tutorial', 'POST', newTutorial);
+        console.log(response);
+        
+        if (response.ok)
+            setNewTutorial(defaultNewTutorial);
     }
 
     return (
@@ -33,19 +42,68 @@ const NewTutorial = () => {
             <div className="tutorial-form wrapper">
                 <div className="app-form">
                     <div className="form-row">
-                        <FormField 
-                            label='Slug'
-                            name='slug'
-                            value={newTutorial.slug}
+                        <div className="form-row">
+                            <FormField 
+                                label='Titre'
+                                name='title'
+                                value={newTutorial.title}
+                                onChange={handleChangeTutorial}
+                            />
+                            <FormField 
+                                label='Slug'
+                                name='slug'
+                                value={newTutorial.slug}
+                                onChange={handleChangeTutorial}
+                            />
+                        </div>
+                        <div className="form-row" style={{ flexGrow: 0 }}>
+                            <FormSelect 
+                                label='Technologie'
+                                name='technology'
+                                value={newTutorial.technology}
+                                options={['React', 'Node.js']}
+                                onChange={handleChangeTutorial}
+                            />
+                            <FormNumber
+                                label='Temps (heures)'
+                                name='hoursToLearn'
+                                value={newTutorial.hoursToLearn}
+                                onChange={handleChangeTutorial}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <FormTextArea 
+                            label='Description'
+                            name='description'
+                            value={newTutorial.description}
                             onChange={handleChangeTutorial}
+                            height={100}
                         />
-                        <FormField 
-                            label='Titre'
-                            name='title'
-                            value={newTutorial.title}
+                    </div>
+                    <div className="form-row">
+                        <FormTextArea 
+                            label='Contenu'
+                            name='content'
+                            value={newTutorial.content}
+                            onChange={handleChangeTutorial}
+                            height={350}
+                        />
+                    </div>
+                    <div className="form-row">
+                        <FormCheckbox 
+                            label='Premium'
+                            name='isPremium'
+                            checked={newTutorial.isPremium}
                             onChange={handleChangeTutorial}
                         />
                     </div>
+                    <button 
+                        onClick={handleSubmitTutorial} 
+                        className='animated filled'
+                    >
+                        Valider
+                    </button>
                 </div>
             </div>
 
@@ -77,7 +135,12 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 const defaultNewTutorial = {
     title: '',
-    slug: ''
+    slug: '',
+    description: '',
+    content: '',
+    technology: 'React',
+    hoursToLearn: 1,
+    isPremium: true
 }
 
 const newTutorialPageSeo: SeoModel = {
