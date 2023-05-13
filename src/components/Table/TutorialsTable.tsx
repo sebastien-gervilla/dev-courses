09f6b5e82debc33e0faa8eef62961ad56c4a92b1
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-05-13 22:18:58 +0200

    Feat: MoreMenu in Tutorial Table

--- src/components/Table/TutorialsTable.tsx
+++ src/components/Table/TutorialsTable.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { Column, Row, Table } from '.';
+import React, { useState, MouseEvent } from 'react';
+import { Column, Table } from '.';
 import { useFetch, useModal } from '@/hooks';
-import { Link, Modal } from '@/components';
+import { ConfirmModal, IconButton, Link, Modal, Popover } from '@/components';
+import { BsThreeDotsVertical } from 'react-icons/bs'
+import { Request } from '@/api';
 
 const TutorialsTable = () => {
 
     const tutorialsRes = useFetch('/tutorial', []);
 
     const modal = useModal();
+    const moreMenu = useModal();
+
+    const [currentRow, setCurrentRow] = useState<CurrentRow>(defaultCurrentRow)
+
+    const deleteTutorial = async () => {
+        const id = currentRow.rowId;
+        if (!id) return;
+
+        const response = await Request.make('/tutorial/' + id, 'DELETE');
+        if (!response.ok) // FEEDBACK SNACKBAR ?
+            return;
+
+        modal.close();
+        tutorialsRes.refresh();
+    }
 
     const columns: Column[] = [
         {
@@ -37,9 +54,21 @@ const TutorialsTable = () => {
         {
             field: 'action',
             title: '',
-            renderCell: (row: Row) => {
+            flex: .1,
+            renderCell: row => {
+                const handleOpenMenu = (event: MouseEvent<HTMLButtonElement>) => {
+                    setCurrentRow({
+                        anchor: event.currentTarget,
+                        rowId: row._id,
+                        slug: row.slug
+                    });
+                    moreMenu.open();
+                }
+
                 return (
-                    <p>OK</p>
+                    <IconButton onClick={handleOpenMenu}>
+                        <BsThreeDotsVertical className='animated' />
+                    </IconButton>
                 )
             }
         }
@@ -48,7 +77,7 @@ const TutorialsTable = () => {
     return (
         <div className='tutorials-table'>
             <Link 
-                href='/admin/new-tutorial'
+                href='/admin/editor'
                 className='animated'
             >
                 Créer un tutoriel
@@ -63,8 +92,58 @@ const TutorialsTable = () => {
                 onClose={modal.close}
                 body={modal.body}
             />
+            <Popover 
+                id='tutorial-table-menu'
+                anchor={currentRow.anchor}
+                isOpen={moreMenu.isOpen}
+                onClose={moreMenu.close}
+                position={{
+                    origin: {
+                        horizontal: 'right',
+                        vertical: 'bottom'
+                    },
+                    transform: {
+                        horizontal: 'right',
+                    },
+                    gap: {
+                        horizontal: 8,
+                        vertical: 24
+                    }
+                }}
+                body={
+                    <div className='menu'>
+                        <Link href={'/admin/editor?slug=' + currentRow.slug}>
+                            Editer
+                        </Link>
+                        <button 
+                            onClick={() => modal.openWith(
+                                <ConfirmModal 
+                                    message='Voulez-vous  vraiment supprimer ce tutoriel ?'
+                                    onCancel={modal.close}
+                                    onConfirm={deleteTutorial}
+                                />
+                            )}
+                        >
+                            Supprimer
+                        </button>
+                    </div>
+                }
+                addArrow
+            />
         </div>
     );
 };
+
+interface CurrentRow {
+    anchor: HTMLButtonElement | null
+    rowId: string
+    slug: string
+}
+
+const defaultCurrentRow: CurrentRow = {
+    anchor: null,
+    rowId: '',
+    slug: ''
+}
 
 export default TutorialsTable;
