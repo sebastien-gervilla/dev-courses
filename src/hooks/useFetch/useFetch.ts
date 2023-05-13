de5d54422487457d02832cb97f0116585cfb0af2
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-05-13 22:19:07 +0200

    Imp: More options on useFetch

--- src/hooks/useFetch/useFetch.ts
+++ src/hooks/useFetch/useFetch.ts
@@ -3,13 +3,25 @@ import { useEffect, useState } from 'react';
 
 export default function useFetch(
         url: URL | RequestInfo, 
-        defaultValue = [], 
+        defaultValue: any = [], 
         options: RequestOptions = defaultOptions
     ) {
 
     const [data, setData] = useState(defaultValue);
 
+    const canFetch = () => {
+        if (!options.requiredValues?.length)
+            return true;
+            
+        return options.requiredValues.some(
+            value => !!value
+        );
+    }
+
     const fetch = async () => {
+        if (!canFetch())
+            return;
+            
         const response = await Request.get(url, options.params);
         setData(response.data || defaultValue);
     }
@@ -25,9 +37,12 @@ export default function useFetch(
 }
 
 interface RequestOptions {
-    params: RequestInit
+    params?: RequestInit
+    requiredValues?: Array<any>
+    onSuccess?: (data: any) => void
 }
 
 const defaultOptions: RequestOptions = {
-    params: {}
+    params: {},
+    requiredValues: []
 }
