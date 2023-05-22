ffacd03e718e7a7ae3d1edfb0bdd5b4b0aea5536
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-05-22 22:46:25 +0200

    Feat: Summary to tutorial

--- src/api/models/Tutorial.ts
+++ src/api/models/Tutorial.ts
@@ -3,6 +3,7 @@ export default interface TutorialModel {
     slug: string,
     title: string,
     description: string,
+    summary: string[],
     content: string,
     technology: string,
     hoursToLearn: number,
