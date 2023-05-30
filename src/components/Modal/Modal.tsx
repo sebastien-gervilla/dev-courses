501ecbe544c87eba2bc3a1e2213834c8376a6c9c
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-05-30 22:06:00 +0200

    Fix: Preveting close when closed

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -12,7 +12,9 @@ const Modal = ({ isOpen, onClose, body }: ModalProps) => {
 
     const modalRef = useRef<HTMLDivElement | null>(null);
 
-    useClickOutside(modalRef, onClose);
+    const closeModal = () => isOpen && onClose();
+
+    useClickOutside(modalRef, closeModal);
 
     const hasMounted = useHasMounted();
 
