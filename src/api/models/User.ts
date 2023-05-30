fc35c5c7602fa6800ee93c9ed63642ccdaf36c83
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-05-30 22:06:18 +0200

    Feat: UserTutorialModel

--- src/api/models/User.ts
+++ src/api/models/User.ts
@@ -1,8 +1,17 @@
+import { TutorialModel } from "."
+
 export default interface UserModel {
-    _id: number,
+    _id: string,
     fname: string,
     lname: string,
     email: string,
     password: string,
     isAdmin: boolean
+}
+
+export interface UserTutorialModel {
+    _id: string,
+    infos: TutorialModel
+    startingDate: Date
+    isComplete: boolean
 }
--- src/api/models/index.ts
+++ src/api/models/index.ts
@@ -1,4 +1,4 @@
 export type { default as ImageModel } from './Image';
 export type { default as SeoModel } from './Seo';
-export type { default as UserModel } from './User';
+export type { default as UserModel, UserTutorialModel } from './User';
 export type { default as TutorialModel } from './Tutorial';
