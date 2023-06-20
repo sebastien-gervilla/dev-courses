91a01c048c715b8d8f224c90a6ff4aa1c5877218
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-06-20 11:43:19 +0200

    Feat: Copyable Field

---
+++ src/components/CopyableField/CopyableField.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Snackbar } from '../Snackbar';
+import { useModal } from '@/hooks';
+
+interface CopyableFieldProps {
+    text: string
+}
+
+const CopyableField = ({ text }: CopyableFieldProps) => {
+
+    const snackbar = useModal();
+
+    const handleCopyField = () => {
+        navigator.clipboard.writeText(text);
+        snackbar.open();
+    };
+
+    return (
+        <div 
+            className='copyable-field'
+            onClick={handleCopyField}
+        >
+            <p>
+                {text}
+            </p>
+            <Snackbar 
+                isOpen={snackbar.isOpen}
+                onClose={snackbar.close}
+                message='Texte copié !'
+                buttonText='Fermer'
+                closeDelay={2500}
+            />
+        </div>
+    );
+};
+
+export default CopyableField;
