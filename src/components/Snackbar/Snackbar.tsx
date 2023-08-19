29cd7fb42f742b618567aa3b950caaacfd46493e
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-08-19 11:48:23 +0200

    Feat: Snackbar styles

--- src/components/Snackbar/Snackbar.tsx
+++ src/components/Snackbar/Snackbar.tsx
@@ -1,16 +1,17 @@
-import React, { CSSProperties, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import { IconButton } from '..';
+import { BsCheckCircle } from 'react-icons/bs';
 
 export interface SnackbarProps {
     isOpen: boolean,
     onClose: () => void,
     message: string,
-    buttonText: string,
     className?: string,
     closeDelay?: number | null
 }
 
-const Snackbar = ({ isOpen, onClose, message, buttonText = 'UNDO', className = '', closeDelay = null }: SnackbarProps) => {
+const Snackbar = ({ isOpen, onClose, message, className = '', closeDelay = null }: SnackbarProps) => {
 
     const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
@@ -30,44 +31,15 @@ const Snackbar = ({ isOpen, onClose, message, buttonText = 'UNDO', className = '
     }
 
     return isOpen ? ReactDOM.createPortal(
-        <div className={"skz-snackbar" + (className ? ` ${className}` : '')} style={_sSnackbar}>
-
-            <p className='skz-snackbar_message' style={_sText}>{message}</p>
-            <button className='skz-snackbar_close-btn' onClick={handleClose} style={_sButton}>{buttonText}</button>
-
+        <div className={'snackbar' + className}>
+            <p>
+                {message}
+            </p>
+            <IconButton onClick={handleClose}>
+                <BsCheckCircle className='animated' />
+            </IconButton>
         </div>
     , document.body) : null;
 };
-
-//#region Styles
-
-const _sSnackbar: CSSProperties = {
-    position: 'fixed',
-    left: '2.5vh',
-    bottom: '2.5vh', 
-    display: 'flex',
-    padding: 15,
-    textAlign: 'left',
-    backgroundColor: '#123',
-    color: 'white',
-    boxShadow: 'rgba(100, 100, 100, 0.35) 0px 7px 29px 0px'
-}
-
-const _sText: CSSProperties = {
-    fontSize: 16,
-    margin: 0,
-    marginRight: 30
-}
-
-const _sButton: CSSProperties = {
-    padding: 0,
-    backgroundColor: 'transparent',
-    color: 'unset',
-    fontSize: 16,
-    border: 'none',
-    cursor: 'pointer'
-}
-
-//#endregion
 
 export default Snackbar;
